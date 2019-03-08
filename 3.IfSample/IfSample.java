fbecf7a588f43c0c62717aade672aae89ad21eee
COMPUTER\vasek <vasika@gmail.com>
2019-03-08 08:11:22 +0200

    First commit

---
+++ 3.IfSample/IfSample.java
@@ -0,0 +1,13 @@
+/* Продемонстрировать применение условного оператора if. Присвоить исходному файлу имя "IfSaiaple.java" */ 
+class IfSample { 
+	public static void main (String args[]) { 
+		int x, y;
+		x = 10; 
+		y  = 20; 
+		if (x <y)   System.out.println("x  меньше  y"); 
+		x = x * 2; 
+		if(x==y) System.out.println("x  теперь равно  y" ); 
+		x = x * 2; 
+		if(x >y)   System.out.println("x  теперь больше y"); // этот оператор  не будет ничего выводить if(x ==у) System.out . println("вы не увидите этого"
+	}
+}
