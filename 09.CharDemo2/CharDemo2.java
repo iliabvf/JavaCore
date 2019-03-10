18f8479aaa93c6d2fc827ab8d36563042a05c6bd
COMPUTER\vasek <vasika@gmail.com>
2019-03-10 11:09:54 +0200

    New examples

---
+++ 09.CharDemo2/CharDemo2.java
@@ -0,0 +1,11 @@
+//Символьные переменные ведут себя как целочисленные значения 
+class CharDemo2 { 
+	public static void main (String args[])   { 
+		char ch1; 
+		ch1 = 'X'; 
+		System.out.println("ch1 содержит"+ ch1); 
+		ch1++; 
+		// увеличить на  единицу значение переменной chl 
+		System.out.println("chl  теперь содержит"  + ch1);
+	}
+}
---
+++ 11.DynInit/DynInit.java
@@ -0,0 +1,9 @@
+// В  этом примере демонстрируется динамическая инициализация 
+class DynInit { 
+	public static void main (String args[])   { 
+		double a = 3.0, b =  4.0; 
+		// динамическая инициализация переменной с 
+		double с = Math.sqrt(a * a + b * b); 
+		System.out.println("Гипoтeнyзa равна" + с);
+	}
+}
