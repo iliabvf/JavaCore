14478ef82dcaa239e07f96abfbcdf37ac41e5022
COMPUTER\vasek <vasika@gmail.com>
2019-04-09 18:02:40 +0300

    new game

---
+++ idea/src/com/iliabvf/javacore/TicTacToe/GameRunner.java
@@ -0,0 +1,9 @@
+package com.iliabvf.javacore.TicTacToe;
+
+// главный класс приложения, содержащий метод main
+public class GameRunner {
+    public static void main(String[] args) {
+        GameLogic gameLogic = new GameLogic();
+        gameLogic.startGame();
+    }
+}
